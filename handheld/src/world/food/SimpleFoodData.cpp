c13d214f138a2ad36ac6c42ed9e918e169f94c94
samesames780 <samesames780@gmail.com>
2026-07-21 23:40:35 -0500

    Update SimpleFoodData.cpp

--- handheld/src/world/food/SimpleFoodData.cpp
+++ handheld/src/world/food/SimpleFoodData.cpp
@@ -4,7 +4,9 @@
 #include "../entity/player/Player.h"
 
 SimpleFoodData::SimpleFoodData()
-:	foodLevel(0)
+:	foodLevel(FoodConstants::MAX_FOOD),
+	exhaustionTimer(0),
+	starveTimer(0)
 {
 }
 
@@ -29,5 +31,24 @@ void SimpleFoodData::tick( Player* player ) {
 			level->raknetInstance->send(packet);
 		}
 		*/
+	}
+
+	// Hunger drain over time: every EXHAUST_TICK_COUNT ticks, lose 1 food point.
+	if (foodLevel > 0) {
+		starveTimer = 0;
+		if (++exhaustionTimer >= FoodConstants::EXHAUST_TICK_COUNT) {
+			exhaustionTimer = 0;
+			foodLevel--;
+		}
+	} else {
+		// Starvation: once food hits 0, deal periodic damage down to a minimum HP,
+		// same pattern as drowning damage in Mob::baseTick (hurt(NULL, dmg)).
+		exhaustionTimer = 0;
+		if (++starveTimer >= FoodConstants::STARVE_TICK_COUNT) {
+			starveTimer = 0;
+			if (player->health > FoodConstants::MIN_STARVE_HEALTH) {
+				player->hurt(NULL, 1);
+			}
+		}
 	}
 }
